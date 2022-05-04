f62861d639b597a6d1d999e95b28b562d6fd0e46
Laura Contreras <m.laura.cp89@gmail.com>
2022-05-04 20:28:27 +0200

    first commit

---
+++ src/main/java/laura/portfolio/reactandspringboot/ReactAndSpringbootApplication.java
@@ -0,0 +1,13 @@
+package laura.portfolio.reactandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/laura/portfolio/reactandspringboot/ReactAndSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package laura.portfolio.reactandspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
